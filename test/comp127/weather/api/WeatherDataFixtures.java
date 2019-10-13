c57e6dbf2b1214b53ce3d78090e9907f81a4c10e
Paul Cantrell <cantrell@pobox.com>
2019-10-13 11:30:36 -0500

    Just (mis)use Random for test fixture generation

--- test/comp127/weather/api/WeatherDataFixtures.java
+++ test/comp127/weather/api/WeatherDataFixtures.java
@@ -3,6 +3,7 @@ package comp127.weather.api;
 import java.time.*;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -23,7 +24,7 @@ public class WeatherDataFixtures {
             generateCurrentConditions(seed),
             IntStream.range(1, Math.abs(seed) * 17 % 39)
                 .mapToObj((offset) ->
-                    generateForecastConditions((offset + 1) * 3, seed + offset))
+                    generateForecastConditions((offset + 1) * 3, seed + offset * 127))
                 .collect(toList()));
     }
 
@@ -49,8 +50,8 @@ public class WeatherDataFixtures {
         return new ForecastConditions(
             predictionTime,
             temperature,
-            temperature - generateDouble(seed, "minTemperature", 1, 20),
-            temperature + generateDouble(seed, "maxTemperature", 1, 20),
+            temperature - 2 * Math.sqrt(hoursFromNow) - generateDouble(seed, "minTemperature", 1, 20),
+            temperature + 2 * Math.sqrt(hoursFromNow) + generateDouble(seed, "maxTemperature", 1, 20),
             generateDouble(seed, "humidity", 0, 100),
             generateDouble(seed, "pressure", 26, 36),
             generateDouble(seed, "cloudCoverage", 0, 100),
@@ -63,16 +64,8 @@ public class WeatherDataFixtures {
     // Helpers to generate stable pseudorandom values so the same seed always generates the same
     // test data
 
-    private static long[] bunchOfPrimes = { 31, 127, 709, 1787, 5381, 8527, 15299, 19577, 27457, 42043, 52711, 72727, 87803, 96797, 112129, 137077, 167449, 173867, 219613, 239489, 250751, 285191, 318211, 352007, 401519, 443419, 464939, 490643, 506683, 527623, 648391, 683873, 718807, 755387, 839483, 864013, 919913, 985151, 1021271, 1080923, 1128889, 1159901, 1254739, 1278779, 1323503, 1342907, 1471343, 1656649, 1693031, 1715761, 1751411, 1793237, 1828669, 1950629, 1993039, 2071583, 2167937, 2193689, 2269733, 2332537, 2364361, 2487943, 2685911, 2750357, 2779781, 2810191, 3042161, 3129913, 3260657, 3284657, 3338989, 3403457, 3509299, 3643579, 3760921, 3829223, 3888551, 3965483, 4030889, 4142053, 4326473, 4348681, 4535189, 4578163, 4658099, 4748047, 4863959, 4989697, 5054303, 5138719, 5182717, 5363167, 5496349, 5587537, 5670851, 5741453, 5823667, 6037513, 6095731, 6415081, 6478961 };
-
     private static int generateInt(int seed, String property, int min, int maxInclusive) {
-        // As seed varies, we want it to cover all the values in the given range. To cycle through
-        // all those values before repeating, we want a seed multiplier that's relatively prime to
-        // the size of the range — which property.hashCode() might not be. So we use the hash code
-        // to grab a prime from an array of primes that itself has a prime length (so the hash codes
-        // sample use all of it), then use _that_ prime as the multiplier for seed.
-        long prime = bunchOfPrimes[Math.abs(property.hashCode()) % bunchOfPrimes.length];
-        return (int) (Math.abs(seed * prime) % (maxInclusive - min + 1) + min);
+        return new Random(seed ^ property.hashCode()).nextInt(maxInclusive - min + 1) + min;
     }
 
     private static double generateDouble(int seed, String property, double min, double maxInclusive) {
