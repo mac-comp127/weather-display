2fcca5dbd8db5694b30517e04fd777da0b955935
Paul Cantrell <cantrell@pobox.com>
2019-10-13 11:02:05 -0500

    Revamped WeatherProgram UI

--- src/comp127/weather/WeatherProgram.java
+++ src/comp127/weather/WeatherProgram.java
@@ -3,71 +3,102 @@ package comp127.weather;
 import comp127.weather.api.OpenWeatherProvider;
 import comp127.weather.widgets.*;
 import comp127graphics.CanvasWindow;
-import comp127graphics.Point;
+import comp127graphics.Rectangle;
 
 import java.awt.Color;
 import java.util.List;
 
-public class WeatherProgram extends CanvasWindow {
+public class WeatherProgram {
 
     //TODO: Replace the value of this string with your own API key.
-    public static final String API_KEY = "d6a22c9835563a57b372e6515fd8ec2b";
+    private static final String API_KEY = "d6a22c9835563a57b372e6515fd8ec2b";
 
-    public static final double     // Location for which we're fetching weather
+    private static final double     // Location for which we're fetching weather
         FORECAST_LAT = 44.936593,  // OLRI 256 (approximate)
         FORECAST_LON = -93.168650;
 
     //TODO: Define an instance variable that is an array of WeatherWidgets. You will also need a variable to keep track
     // of which index in the array is the widget that is currently displayed.
-    private List<WeatherWidget> widgets;
+    private CanvasWindow canvas;
+    double miniWidgetSize, largeWidgetSize;
+    private List<WeatherWidget> miniWidgets, largeWidgets;
+    private WeatherWidget displayedLargeWidget;
+    private Rectangle selectionHighlight;
     private int currentDisplayIndex = 0;
 
-    public WeatherProgram() {
-        super("Weather Display", 800, 600);
+    public WeatherProgram(double largeWidgetSize) {
+        this.largeWidgetSize = largeWidgetSize;
+        largeWidgets = createWidgets(largeWidgetSize);
 
-        Point widgetSize = new Point(getWidth(), getHeight());
+        miniWidgetSize = largeWidgetSize / largeWidgets.size();  // so they stack along one edge
+        miniWidgets = createWidgets(miniWidgetSize);
 
-        //TODO: Initialize your WeatherWidget array and add widgets to it. Add the first widget to the canvas so that it appears
-        widgets = List.of(
-            new ForecastGraphWidget(getWidth()),
-            new TemperatureWidget(getWidth()),
-            new ForecastWidget(getWidth()),
-            new WindWidget(getWidth())
-        );
+        canvas = new CanvasWindow(
+            "Weather Display",
+            (int) Math.round(largeWidgetSize + miniWidgetSize),
+            (int) Math.round(largeWidgetSize));
+        canvas.setBackground(new Color(153, 204, 255));
 
-        add(widgets.get(0).getGraphics());
+        selectionHighlight = new Rectangle(0, 0, miniWidgetSize, miniWidgetSize);  // selectWidgetAtIndex() will position it
+        selectionHighlight.setStroked(false);
+        selectionHighlight.setFillColor(new Color(0x7FFFFFFF, true));
+        canvas.add(selectionHighlight);
+
+        double y = 0;
+        for (WeatherWidget widget : miniWidgets) {
+            canvas.add(widget.getGraphics(), largeWidgetSize, y);
+            y += miniWidgetSize;
+        }
+        selectWidgetAtIndex(0);
 
         new OpenWeatherProvider(API_KEY, FORECAST_LAT, FORECAST_LON)
             .fetchWeather((weatherData) -> {
-                for (WeatherWidget widget : widgets) {
+                for (WeatherWidget widget : miniWidgets) {
                     widget.update(weatherData);
                 }
-                draw();
+                for (WeatherWidget widget : largeWidgets) {
+                    widget.update(weatherData);
+                }
+                canvas.draw();
             });
 
-        //TODO: Implement MouseMotion Listeners. When the mouse is moved you should:
-        // 1. Check if your ForecastWidget is currently displayed.
-        // 2. If so, you should update which forecast is displayed depending on which ForecastBox the mouse is currently hovering over.
-
-        onMouseMove(event -> {
-            widgets.get(currentDisplayIndex).onHover(event.getPosition());
+        canvas.onMouseMove(event -> {
+            if (displayedLargeWidget != null && event.getPosition().getX() < largeWidgetSize) {
+                displayedLargeWidget.onHover(event.getPosition());
+            }
         });
 
-        //TODO: Implement Mouse Listeners. When the mouse is clicked you should:
-        // 1. remove everything from the canvas
-        // 2. Advance your variable for which index is currently displayed. Keep in mind that it should cycle back to 0 when it gets to the end of the array.
-        // 3. Add the new current widget to the canvas.
-
-        onClick(event -> {
-            currentDisplayIndex = (currentDisplayIndex + 1) % widgets.size();
-            removeAll();
-            add(widgets.get(currentDisplayIndex).getGraphics());
+        canvas.onClick(event -> {
+            if (event.getPosition().getX() >= largeWidgetSize) {
+                selectWidgetAtIndex(
+                    (int) (event.getPosition().getY() / largeWidgetSize * miniWidgets.size()));
+            }
         });
+    }
 
-        setBackground(new Color(153, 204, 255));
+    private List<WeatherWidget> createWidgets(double size) {
+        return List.of(
+            new TemperatureWidget(size),
+            new WindWidget(size),
+            new ForecastWidget(size),
+            new ForecastGraphWidget(size));
+    }
+
+    private void selectWidgetAtIndex(int index) {
+        if (index < 0 || index >= largeWidgets.size()) {
+            return;
+        }
+
+        if (displayedLargeWidget != null) {
+            canvas.remove(displayedLargeWidget.getGraphics());
+        }
+        displayedLargeWidget = largeWidgets.get(index);
+        canvas.add(displayedLargeWidget.getGraphics());
+
+        selectionHighlight.setPosition(largeWidgetSize, miniWidgetSize * index);
     }
 
     public static void main(String[] args) {
-        new WeatherProgram();
+        new WeatherProgram(600);
     }
 }
